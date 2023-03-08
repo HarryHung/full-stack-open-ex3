ae1a3261161ac1b3ae90e860b99d669e644d0c69
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-03-08 01:29:39 +0000

    exercise 3.5

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+      id: 1,
+      name: "Arto Hellas", 
+      number: "040-123456"
     },
     { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+      id: 2,
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
     },
     { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+      id: 3,
+      name: "Dan Abramov", 
+      number: "12-43-234345"
     },
     { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+      id: 4,
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
     }
 ]
 
@@ -44,6 +46,28 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
   
     response.status(204).end()
+})
+
+const generateId = () => Math.floor(Math.random() * 1000000000)
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+    
+    response.json(person)
 })
 
 app.get('/info', (request, response) => {
