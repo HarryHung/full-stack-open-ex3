075a996eb3d4d224551f5344962a3a33670d2bad
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-03-12 20:09:14 +0000

    exercise 3.15-3.18

--- index.js
+++ index.js
@@ -14,25 +14,31 @@ morgan.token('json', (request, response) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+    Person.find({})
+        .then(persons => {
+            response.json(persons)
+        })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person){
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-
-const generateId = () => Math.floor(Math.random() * 1000000000)
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -51,7 +57,7 @@ app.post('/api/persons', (request, response) => {
 
     const person = new Person({
         name: body.name,
-        number: body.number,
+        number: body.number
     })
     
     person.save().then(savedPerson => {
@@ -59,15 +65,45 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.get('/info', (request, response) => {
-    const count = persons.length
-    const time = new Date()
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
 
-    response.send(`
-        <p>Phonebook has info for ${count} people</p>
-        <p>${time}</p>
-    `)
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+    
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
+
+app.get('/info', (request, response) => {
+    Person.find({})
+        .then(persons => {
+            const count = persons.length
+            const time = new Date()
+
+            response.send(`
+                <p>Phonebook has info for ${count} people</p>
+                <p>${time}</p>
+            `)
+        })
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+  
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
