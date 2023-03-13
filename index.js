a1306db1c1f102596a2d525e8364722f7ba4a77c
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-03-13 00:14:10 +0000

    exercise 3.19

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     
     if (!body.name) {
@@ -60,9 +60,11 @@ app.post('/api/persons', (request, response) => {
         number: body.number
     })
     
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -73,7 +75,11 @@ app.put('/api/persons/:id', (request, response, next) => {
         number: body.number
     }
     
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    Person.findByIdAndUpdate(
+        request.params.id,
+        person,
+        {new: true, runValidators: true, context:'query'}
+    )    
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -97,8 +103,10 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
   
     if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } 
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message})
+    }
   
     next(error)
 }
